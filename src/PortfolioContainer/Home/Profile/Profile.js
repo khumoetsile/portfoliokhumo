4c83a0482c4228797456d41eefcb5aa9cc227113
Khumoetsile Molabi <KMolabi@bitri.co.bw>
2022-01-17 16:49:33 +0200

    Second Commit

---
+++ src/PortfolioContainer/Home/Profile/Profile.js
@@ -0,0 +1,75 @@
+import React from "react";
+import Typical from "react-typical";
+import "./Profile.css"
+
+export default function Profile() {
+  return (
+    <div className="profile-container">
+      <div className="profile-parent">
+        <div className="profile-details">
+          <div className="colz">
+            <div className="colz-icon">
+            <a href="#">
+              <i className="fa fa-facebook-square"></i>
+            </a>
+            <a href="#">
+              <i className="fa fa-google-plus-square"></i>
+            </a>
+            <a href="#">
+              <i className="fa fa-instagram"></i>
+            </a>
+            <a href="#">
+              <i className="fa fa-youtube-square"></i>
+            </a>
+            <a href="#">
+              <i className="fa fa-twitter"></i>
+            </a>
+            </div>
+          </div>
+          <div className="profile-details-name">
+            <span className="primary-text">
+              {" "}
+              Hello, I am{" "}
+              <span className="highlited-text">Khumoetsile Molabi</span>
+            </span>
+          </div>
+          <div className="profile-details-role">
+            <span className="primary-text">
+              {" "}
+              <h1>
+                {" "}
+                <Typical
+                  loop={Infinity}
+                  steps={[
+                    "Frontend Developer 😃",
+                    2000,
+                    "Backend Developer 🤩",
+                    2000,
+                    "UI/UX Designer 😎",
+                    2000,
+                  ]}
+                />
+              </h1>
+              <span className="profile-role-tagline">
+                I am an exceptionally creative and dependable Software Developer
+                with superb work ethic.
+              </span>
+            </span>
+          </div>
+          <div className="profile-options">
+            <button className="btn primary-btn">
+              {""}
+              Hire Me{" "}
+            </button>
+            <a href="Curriculum_Vitae.pdf" download="Curriculum_Vitae.pdf">
+              <button className="btn highlighted-btn">Get Resume</button>
+            </a>
+          </div>
+        </div>
+        <div className="profile-picture">
+          <div className="profile-picture-background"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
